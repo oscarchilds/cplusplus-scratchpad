53b7a5fb2408058fe35521f6e55554c9c33a6d40
Oscar Childs <oscarjchilds@gmail.com>
2026-08-19 07:46:13 +0100

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << sizeof(int) << '\n';
+    return 0;
+}
+
+// int main()
+// {
+//     cout << "Enter three numbers: ";
+
+//     int x{}, y{}, z{};
+//     cin >> x >> y >> z;
+
+//     cout << "You entered: " << x << ", " << y << ", and " << z << ".\n";
+//     return 0;
+// }
+
+// int main()
+// {
+//     cout << "Enter two numbers: ";
+
+//     int x{};
+//     cin >> x;
+
+//     int y{};
+//     cin >> y;
+
+//     cout << "You entered: " << x << " and " << y << '\n';
+//     return 0;
+// }
+
+// int main()
+// {
+//     cout << "Enter two numbers seperated by a space: ";
+
+//     int x{};
+//     int y{};
+//     cin >> x >> y;
+
+//     cout << "You entered: " << x << " and " << y << '\n';
+//     return 0;
+// }
+
+// int main()
+// {
+//     int width = 5;
+
+//     cout << "The value of width is: " << width << "\n";
+
+//     width = 7;
+
+//     cout << "The value of width is: " << width << "\n";
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     vector<string> msg{"Hello", "C++", "World"};
+
+//     for (const string& word : msg)
+//     {
+//         cout << word << " ";
+//     }
+//     cout << endl;
+
+//     std::cout << 5;       // print the literal number `5`
+//     std::cout << -6.7;    // print the literal number `-6.7`
+//     std::cout << 'H';     // print the literal character `H`
+//     std::cout << "Hello"; // print the literal text `Hello`
+
+//     return 0;
+// }
